b5d0c16915bd3d1414c7db4744919e40ec13066a
Charles Schiermeyer <matkatmusic@gmail.com>
2018-09-04 15:50:19 -0700

    initial commit

---
+++ Source/ScopedValueSaver.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    ScopedValueSaver.h
+    Created: 4 Sep 2018 3:48:44pm
+    Author:  Charles Schiermeyer
+
+  ==============================================================================
+*/
+
+#ifndef SCOPEDVALUESAVER_H_INCLUDED
+#define SCOPEDVALUESAVER_H_INCLUDED
+
+
+
+
+
+#endif  // SCOPEDVALUESAVER_H_INCLUDED
